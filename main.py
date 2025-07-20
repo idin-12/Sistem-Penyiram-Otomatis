4aaa09247a31589f0baaa7ec756dad44f87febf6
idin-12 <trsrhdh@gmail.com>
2025-07-20 12:42:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ h3 {
 # 💧 Judul Aplikasi
 st.markdown("# 💧 Sistem Penyiram Otomatis 💧")
 st.markdown("### Smart Sprinkler System Controller")
-st.markdown("#### Muhyiddin As Syarif (312410122")
+st.markdown("# Muhyiddin As Syarif (312410122")
 
 st.write("---")
 
